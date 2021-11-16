ef600d67d57e50ef9464f4227c854f0bd431b4b3
adriano <adriano08andrade@hotmail.com>
2021-11-16 12:00:57 -0300

    Correção da rota deletar

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('add/', CreateProdutoView.as_view(), name='add_produto'),
     path('<int:pk>/update/', UpdateProdutoView.as_view(), name='upd_produto'),
-    path('<int:pk>/update/', DeletaProdutoView.as_view(), name='del_produto'),
+    path('<int:pk>/del/', DeletaProdutoView.as_view(), name='del_produto'),
 ]
