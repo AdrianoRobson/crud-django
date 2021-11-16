2801468ca5813328fc2a2ca0f88aec2b07a452e8
adriano <adriano08andrade@hotmail.com>
2021-11-16 09:05:43 -0300

    Criação da view IndexView utilizando o class based view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Produto
+from .forms import ProdutoModelForm
+
+
+class IndexView(ListView):
+    models = Produto
+
+    # Template
+    template_name = 'index.html'
+
+    # Consulta no banco de dados
+    queryset = Produto.objects.all()
+
+    # Recuperar dados do template
+    context_object_name = 'produtos'
+
