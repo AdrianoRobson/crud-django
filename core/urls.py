367ce68671f35fdcc802ff38ca62b069eb3c0b15
adriano <adriano08andrade@hotmail.com>
2021-11-16 10:03:22 -0300

    Criação da rota para adicionar produto

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, CreateProdutoView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index')
+    path('', IndexView.as_view(), name='index'),
+    path('add/', CreateProdutoView.as_view(), name='add_produto'),
 ]
