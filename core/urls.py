0391840727c38fda421173cdd79e85e3583bf7a9
adriano <adriano08andrade@hotmail.com>
2021-11-16 10:09:41 -0300

    Adicionando a rota de atualização do produto

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView, CreateProdutoView
+from .views import IndexView, CreateProdutoView, UpdateProdutoView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('add/', CreateProdutoView.as_view(), name='add_produto'),
+    path('<int:pk>/update/', UpdateProdutoView.as_view(), name='upd_produto')
 ]
